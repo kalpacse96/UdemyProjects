7b28935f414a37386916db2b7cb9d41ae8c37345
Kalpataru Sahoo <kalpacse96@gmail.com>
2025-07-22 09:15:18 +0530

    Testing commit for AsyncImage

---
+++ AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Kalpataru Sahoo on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
