0e36914e8140faa7db5b814579dc21513bb0ca09
Kalpataru Sahoo <kalpacse96@gmail.com>
2025-07-22 08:50:33 +0530

    Initial Commit

---
+++ TextColor/TextColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextColorApp.swift
+//  TextColor
+//
+//  Created by Kalpataru Sahoo on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TextColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
